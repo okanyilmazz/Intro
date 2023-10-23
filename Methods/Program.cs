e5328c95b8c88d5256216d922a411de5173e31a8
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-24 00:03:57 +0300

    Ref Keyword

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,12 +1,20 @@
 ﻿
-Add();
-Add();
-Add();
-Add();
-var result = Add2(20, 30);
-var defaultResult = Add2();
-Console.WriteLine(result);
-Console.WriteLine(defaultResult);
+//Add();
+//Add();
+//Add();
+//Add();
+//var result = Add2(20, 30);
+
+//var defaultResult = Add2();
+//Console.WriteLine(defaultResult);
+
+int number1 = 20;
+int number2 = 100;
+
+var result2 = Add3(ref number1, number2);
+Console.WriteLine(result2);
+Console.WriteLine(number1);
+
 Console.ReadLine();
 
 
@@ -22,4 +30,12 @@ static int Add2(int number1 = 20, int number2 = 30)
 {
     var result = number1 + number2;
     return result;
+}
+
+// ref, Değer tiplerini referans tip gibi kullanmaya yarar
+
+static int Add3(ref int number1, int number2)
+{
+    number1 = 30;
+    return number1 + number2;
 }
