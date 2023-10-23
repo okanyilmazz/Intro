29df41b9c0461eb0dea5e04b78a2709a0aa12133
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-23 21:11:20 +0300

    If Blocks

---
+++ Conditionals/Program.cs
@@ -0,0 +1,15 @@
+﻿// Mantıksal Operatörler : && ve , || veya , ! değil
+// Karşılaştırma Operatörler : < küçüktür, > büyüktür, <= küçük veya eşittir, >= büyük veya eşittir, == eşittir, != eşit değildir
+
+var number = 10;
+if (number == 10)
+{
+    Console.WriteLine("Number is 10");
+
+}
+else
+{
+    Console.WriteLine("Number is not 10");
+}
+
+Console.ReadLine();
