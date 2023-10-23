1e6bfa188fbc29923e7b91c31311b584d373ce01
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-23 23:19:36 +0300

    Else If Blocks

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -18,3 +18,18 @@ else
 
 Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
 Console.ReadLine();
+
+// Else if
+// Else olduğu için mutlaka biri çalışacak
+if (number == 10)
+{
+    Console.WriteLine("Number is 10");
+}
+else if (number == 20)
+{
+    Console.WriteLine("Number is 20");
+}
+else
+{
+    Console.WriteLine("Number is not 10 or 20");
+}
