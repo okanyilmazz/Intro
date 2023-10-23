891bf611645f57ffd351a90e8529172e6d43feb1
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-24 00:57:15 +0300

    Sample Application: Prime Number

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,13 +1,33 @@
-﻿string[] students = new string[3] { "Engin", "Derin", "Salih" };
-foreach (var student in students)
-{
-    Console.WriteLine(student);
-}
+﻿//ForEachLoop();
 
 //DoWhileLoop();
 //WhileLoop();
 //ForLoop();
 
+
+if (IsPrimeNumber(7))
+{
+    Console.WriteLine("This is a prime number");
+}
+else
+{
+    Console.WriteLine("This is not a prime number");
+}
+
+
+static bool IsPrimeNumber(int number)
+{
+    bool result = true;
+    for (int i = 2; i < number - 1; i++)
+    {
+        if (number % i == 0)
+        {
+            result = false;
+            i = number;
+        }
+    }
+    return result;
+}
 
 static void ForLoop()
 {
@@ -45,4 +65,13 @@ static void DoWhileLoop()
         Console.WriteLine(number);
         number--;
     } while (number >= 11);
+}
+
+static void ForEachLoop()
+{
+    string[] students = new string[3] { "Engin", "Derin", "Salih" };
+    foreach (var student in students)
+    {
+        Console.WriteLine(student);
+    }
 }
