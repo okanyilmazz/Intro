a0de7807ebcb24b4780dbd1263f5eedb64164415
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-24 00:25:49 +0300

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] students = new string[3] { "Engin", "Derin", "Salih" }; ;
+//students[0] = "Engin";
+//students[1] = "Derin";
+//students[2] = "Salih";
+
+string[] students2 = { "Engin", "Derin", "Salih" };
+// Verilen eleman sayısından fazla ekleyemiyoruz.
+students2[3] = "Ahmet";
+
+foreach (var student in students2)
+{
+    Console.WriteLine(student);
+}
+Console.WriteLine();
+Console.ReadLine();
