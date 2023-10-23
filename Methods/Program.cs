16ff832497a26eec9776e037912bb6b66e8b7588
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-23 23:49:17 +0300

    Parameterized Method

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -3,12 +3,18 @@ Add();
 Add();
 Add();
 Add();
-
-
+var result = Add2(20, 30);
+Console.WriteLine(result);
 Console.ReadLine();
 
 
 static void Add()
 {
     Console.WriteLine("Added!!!");
+}
+
+static int Add2(int number1, int number2)
+{
+    var result = number1 + number2;
+    return result;
 }
