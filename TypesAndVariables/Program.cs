f4aae347a7bc411314f4fdbb9f389b579002fe5e
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-23 20:33:21 +0300

    Decimal Data Type

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -1,4 +1,4 @@
-﻿//Value Types
+﻿// Value Types
 
 // int sınırı = -2147483648 ~ 214748347 aralığındadır.
 // 32 bit yer kaplar
@@ -6,8 +6,8 @@
 int number1 = 10;
 Console.WriteLine("Number1 is {0}", number1);
 
-//-9223372036854775808 ~ 9223372036854775807 aralığındadır.
-//64 bit yer kaplar
+// -9223372036854775808 ~ 9223372036854775807 aralığındadır.
+// 64 bit yer kaplar
 
 long number2 = 214748347;
 Console.WriteLine("Number1 is {0}", number2);
@@ -31,15 +31,24 @@ Console.WriteLine("Number4 is {0}", number4);
 
 bool condition = true;
 
-//int değerine çevrilebilir.
-//Dönüşüm esnasında ASCII sayısal karşılığını bize verir
+// int değerine çevrilebilir.
+// Dönüşüm esnasında ASCII sayısal karşılığını bize verir
 
 char character = 'A';
 Console.WriteLine("Character is : {0}", character);
 Console.WriteLine("Character is : {0}", (int)character);
 
-//Ondalıklı sayıları tutar
+// Ondalıklı sayıları tutar
 // 64 bit yer kaplar
+// Virgülden sonra 15-16 karakter tutar
 
 double number5 = 10.4;
-Console.WriteLine(number5);
+Console.WriteLine("Number5 is {0}", number5);
+
+// Int - Long ilişkisi double - decimal içinde geçerlidir.
+// Hassas değerler için kullanabilir (Para vs.)
+// Virgülden sonra 28-29 karakter tutar.
+// Ondalıklı yazmak istediğimizde " m " veya " M " harfini sonuna getirmeliyiz.
+
+decimal number6 = 10.4m;
+Console.WriteLine("Number6 is {0}", number6);
