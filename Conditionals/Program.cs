72e4dc79713ae099fd3908d17ae3437b1496216e
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-23 23:25:07 +0300

    Switch Blocks

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -33,3 +33,21 @@ else
 {
     Console.WriteLine("Number is not 10 or 20");
 }
+
+// Switch
+
+var numberSwitch = 11;
+
+switch (numberSwitch)
+{
+    case 10:
+        Console.WriteLine("Number is 10");
+        break;
+    case 20:
+        Console.WriteLine("Number is 20");
+        break;
+
+    default:
+        Console.WriteLine("Number is not 10 or 20");
+        break;
+}
