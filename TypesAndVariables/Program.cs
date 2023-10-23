91ba6b65185eb1d45172523aaf96aae8423f0b5d
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-23 20:16:01 +0300

    Long Data Type

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -5,3 +5,9 @@
 
 int number1 = 10;
 Console.WriteLine("Number1 is {0}", number1);
+
+//-9223372036854775808 ~ 9223372036854775807 aralığındadır.
+//64 bit yer kaplar
+
+long number2 = 214748347;
+Console.WriteLine("Number1 is {0}", number2);
