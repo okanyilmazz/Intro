20163065d5a99a433047b3636acb7e9d5686a6bf
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-24 01:01:33 +0300

    Strings

---
+++ Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿string city = "Ankara";
+
+Console.WriteLine(city[0]);
+
+foreach (var item in city)
+{
+    Console.WriteLine(item);
+}
+
+string city2 = "İstanbul";
+
+//string result = city + city2;
+//Console.WriteLine(result);
+
+Console.WriteLine(String.Format("{0} {1}", city, city2));
+
+Console.ReadLine();
