3b5f0cac3b60aa6a6330ca402e3447c2fa02cd9a
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-23 23:37:17 +0300

    Multiple Conditions

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -2,6 +2,7 @@
 // Karşılaştırma Operatörler : < küçüktür, > büyüktür, <= küçük veya eşittir, >= büyük veya eşittir, == eşittir, != eşit değildir
 
 var number = 10;
+
 if (number == 10)
 {
     Console.WriteLine("Number is 10");
@@ -17,7 +18,7 @@ else
 //                               True           : False
 
 Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
-Console.ReadLine();
+
 
 // Else if
 // Else olduğu için mutlaka biri çalışacak
@@ -51,3 +52,20 @@ switch (numberSwitch)
         Console.WriteLine("Number is not 10 or 20");
         break;
 }
+
+// Multiple Conditions
+
+if (number >= 0 && number <= 100)
+{
+    Console.WriteLine("Number is between 0-100");
+}
+else if (number > 100 && number <= 200)
+{
+    Console.WriteLine("Number is between 101-200");
+}
+else if (number > 200 || number < 0)
+{
+    Console.WriteLine("Number is less than 0 or greater than 200");
+}
+
+Console.ReadLine();
