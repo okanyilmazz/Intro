984a3ff5b61b76d15117cf1c45a7830d3c59b41a
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-24 00:40:03 +0300

    For Loop

---
+++ Loops/Program.cs
@@ -0,0 +1,11 @@
+﻿for (int i = 2; i <= 100; i += 2)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = 100; i >= 0; i -= 2)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("Finished!");
+Console.ReadLine();
