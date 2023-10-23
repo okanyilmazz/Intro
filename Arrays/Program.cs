9045730eae6df9013bb68aa6d0ac43db3901bc1a
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-24 00:33:38 +0300

    Multidimensional Arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -5,11 +5,32 @@
 
 string[] students2 = { "Engin", "Derin", "Salih" };
 // Verilen eleman sayısından fazla ekleyemiyoruz.
-students2[3] = "Ahmet";
 
-foreach (var student in students2)
+//students2[3] = "Ahmet";
+
+//foreach (var student in students2)
+//{
+//    Console.WriteLine(student);
+//}
+
+// [satır,sütun]
+
+string[,] regions = new string[5, 3]
 {
-    Console.WriteLine(student);
+    {"İstanbul","İzmit","Balıkesir" },
+    {"Ankara","Konya","Kırıkkale" },
+    {"Antalya","Adana","Mersin" },
+    {"Rize","Trabzon","Samsun" },
+    {"İzmir","Muğla","Manisa" }
+};
+//Satırlar 0. Dimension, Sütunlar 1. Dimension
+
+for (int i = 0; i <= regions.GetUpperBound(0); i++)
+{
+    for (int j = 0; j <= regions.GetUpperBound(1); j++)
+    {
+        Console.WriteLine(regions[i, j]);
+    }
+    Console.WriteLine("****************");
 }
-Console.WriteLine();
 Console.ReadLine();
