ac026d154bdf89b271376bad76b504c24bb5e452
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-26 01:14:35 +0300

    Creating a Recap Demo Checkers Board

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -8,19 +8,9 @@ namespace Classes
 {
     public class Customer
     {
-        private string _firstName;
+
         public int Id { get; set; } //Auto property
-        public string FirstName
-        {
-            get
-            {
-                return "Mrs." + _firstName;
-            }
-            set
-            {
-                _firstName = value;
-            }
-        }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string City { get; set; }
     }
