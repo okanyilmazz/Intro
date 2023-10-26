bcaa5e5277a979dd86fd84f9c7da516219ead719
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-26 13:52:10 +0300

    Interfaces - 2

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -4,14 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PersonManager manager = new PersonManager();
+
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Engin",
+                LastName = "Demiroğ",
+                Address = "Ankara"
+            };
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "Derin",
+                LastName = "Demiroğ",
+                Departmant = "Computer Sciences"
+            };
+            manager.Add(customer);
+            manager.Add(student);
         }
     }
 
     //soyut nesne
     interface IPerson
     {
-         int Id { get; set; }
+        int Id { get; set; }
         string FirstName { get; set; }
         string LastName { get; set; }
     }
@@ -23,11 +40,26 @@
         public string LastName { get; set; }
         public string Address { get; set; }
     }
-    class Student:IPerson
+    class Student : IPerson
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Departmant { get; set; }
+    }
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
     }
 }
