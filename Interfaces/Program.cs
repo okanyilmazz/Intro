bfaeb8e26a39aabe2aaee6d689372fe100052740
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-26 13:28:36 +0300

    Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    //soyut nesne
+    interface IPerson
+    {
+         int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+    //somut nesne
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+    class Student:IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+}
