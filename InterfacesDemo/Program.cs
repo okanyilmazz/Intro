ba509ea5258d2a0d855c8440f5e941f0235188bf
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-10-26 14:51:01 +0300

    Multiple Implementation

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace InterfacesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
+            };
+
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+
+            IEat[] eats = new IEat[2]
+            {
+                new Worker(),
+                new Manager()
+            };
+            foreach (var eat in eats)
+            {
+                eat.Eat();
+            }
+        }
+    }
+
+    interface IWorker
+    {
+        void Work();
+    }
+    
+    //SOLID, Interface Segregation
+
+    interface IEat
+    {
+        void Eat();
+    }
+
+    interface ISalary
+    {
+        void GetSalary();
+    }
+
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Manager Eat");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Manager Get Salary");
+
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Manager Work");
+
+        }
+    }
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
